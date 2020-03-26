7c12b5d049114999a728fe5c70bd50b61317d680
Evan ONeil <evan@evanoneil.net>
2020-03-26 12:28:56 -0500

    3.26 video page CMS

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -139,7 +139,10 @@ const IndexPage = props => {
         <meta name="twitter:title" content={site.title} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:description" content={site.description} />
-        <meta property="twitter:image" content={OGImage} />
+        <meta
+          property="twitter:image"
+          content="https://environmental-youth-council.netlify.com/static/og-39aa5b62b7b76ab64ac4a6154fe2f72c.jpg"
+        />
       </Helmet>
       {/* <SEO title={site.title} description={site.description} keywords={site.keywords} /> */}
       <Subheader nodes={aboutNodes} focusNodes={focusNodes} />
